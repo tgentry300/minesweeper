076307e4be71065bbef83cd82bad1f7002f47135
Taylor Gentry <taylor.gentry300@gmail.com>
2018-05-24 16:23:20 -0400

    committed again

--- main.js
+++ main.js
@@ -1,20 +1,57 @@
-const gameBoardCreate = {
+const config = {
     main: document.getElementById("main"),
     numberOfRows: 10,
     numberOfColumns: 10,
     numberOfMines: 10,
-    gameGridElementsArray: [],
-    gameGridArray: function () {
+}
+
+const GameBoard = function (config) {
+    this.main = config.main
+    this.numberOfRows = config.numberOfRows
+    this.numberOfColumns = config.numberOfColumns
+    this.numberOfMines = config.numberOfMines
+
+    this.gameGridElementsArray = []
+
+    this.assignEventListeners()
+    this.drawBoard()
+    this.drawMines()
+}
+
+GameBoard.prototype = {
+
+    eventListeners: {
+
+        click: function (event) {
+            if (!event.target.classList.contains("cell")) return;
+
+            if (event.target.childElementCount) {
+                event.target.firstChild.style.visibility = "visible"
+            } else {
+                //PUT THINGS HERE
+                event.target.style.backgroundColor = "lightsalmon"
+            }
+        },
+        contextClick: function (event) {
+            if (!event.target.classList.contains("cell")) return;
+
+            // ... do the flag shit
+        },
+
+    },
+
+    createGameGridArray: function () {
         return Array(this.numberOfRows).fill('0').map(() => new Array(this.numberOfColumns).fill('0'))
     },
+
     drawBoard: function () {
         // console.log(this.gameGridArray())
-        for (let colIndex in this.gameGridArray()) {
+        for (let colIndex in this.createGameGridArray()) {
             this.gameGridElementsArray.push([])
             const colElement = document.createElement("div")
             colElement.classList.add("column")
             this.main.appendChild(colElement)
-            for (let cellIndex in this.gameGridArray()[colIndex]) {
+            for (let cellIndex in this.createGameGridArray()[colIndex]) {
                 const cellElement = document.createElement("div")
                 cellElement.classList.add("cell")
                 cellElement.dataset.columns = colIndex
@@ -34,11 +71,6 @@ const gameBoardCreate = {
         return Math.floor(Math.random() * Math.floor(this.numberOfColumns - 1));
     },
 
-
-
-
-
-
     drawMines: function () {
         for (let i = 0; i < this.numberOfMines; i++) {
             const colIndex = this.getRandomIntCol()
@@ -57,6 +89,12 @@ const gameBoardCreate = {
             }
         }
     },
+
+    assignEventListeners: function () {
+        this.main.addEventListener("click", this.eventListeners.click)
+        this.main.addEventListener("contextmenu", this.eventListeners.contextClick)
+    },
+
 }
-gameBoardCreate.drawBoard()
-gameBoardCreate.drawMines()
+
+const gameBoard = new GameBoard(config)
